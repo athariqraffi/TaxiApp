e4143aaef8e8cceb1197342c1181496cb87ae559
athariqraffi <athariq1234@gmail.com>
2021-03-16 16:45:02 +0800

    Push Taxi

---
+++ Taxi.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tugasTaxi
+{
+    class Taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+        
+            Console.WriteLine("Driver Name          : {0}", DriverName);
+
+            if (OnDuty == true) {
+                Console.WriteLine("On Duty              : Yes", OnDuty);
+            } else { 
+                Console.WriteLine("On Duty              : No", OnDuty);
+            }
+
+            Console.WriteLine("Number Of Passengeer : {0}\n", NumPassenger);
+
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", DriverName);
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai menjemput penumpang\n", DriverName);
+        }
+    }
+}
